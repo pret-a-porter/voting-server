1b4755eddc3aced0e5cf644103a020e036c110dc
Maksim Sharipov <m.sharipov@semrush.com>
2016-08-09 15:47:57 +0300

    Init core

---
+++ test/core_spec.js
@@ -0,0 +1,30 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries} from '../src/core';
+
+describe('application logic', () => {
+
+  describe('setEntries', () => {
+
+    it('добавляет записи к состоянию', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+    it('преобразует в immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+  });
+
+});
